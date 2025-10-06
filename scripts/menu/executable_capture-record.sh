890ab7c7056dffa2985d2be8db2d90787c37b541
Estherenne <cyclone278@proton.me>
2025-10-06 10:25:45 +0200

    recording menu

---
+++ scripts/menu/executable_capture-record.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SELECTION="$(printf "󰀁 Record region\n󰖲 Record window\n󰍹 Record output\n󰙦 Stop recording" | rofi -dmenu -theme-str "window { height: 215px; }")"
+case $SELECTION in
+	*"Record region")
+		~/scripts/record.sh region;;
+	*"Record window")
+		~/scripts/record.sh window;;
+	*"Record output")
+		~/scripts/record.sh output;;
+  *"Stop recording")
+    pkill wl-screenrec;;
+esac
